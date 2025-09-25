f5fa6bf885c84e9bd4d5ab75e9cc93462be3218d
haoo192 <234910126+haoanhh192@users.noreply.github.com>
2025-09-25 12:00:00 +0700

    Shield Punch and bomb nerf

--- Assets/EnableHitVFX.cs
+++ Assets/EnableHitVFX.cs
@@ -1,11 +1,19 @@
+using D2D;
+using DG.Tweening;
 using UnityEngine;
 
 public class EnableHitVFX : MonoBehaviour
 {
     [SerializeField] private ParticleSystem VFX;
 
+    private Tween tween;
+
     private void OnTriggerEnter(Collider collision)
     {
         VFX?.Play();
+
+        tween.KillTo0();
+
+        tween = transform.DOPunchScale(Vector3.one * 0.005f, .2f);
     }
 }
